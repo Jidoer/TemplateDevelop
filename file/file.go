378c2280c3867270512fe1767c39b3dbc397fa78
John_c <52993727+kangc666@users.noreply.github.com>
2022-01-20 10:22:30 +0800

    Update

--- file/file.go
+++ file/file.go
@@ -1,65 +1,45 @@
 package file
+
 import (
 	"io/ioutil"
-	// "fmt"
 	"os"
 )
- 
- 
-func Create(fileName string,nr string) {
-   content := []byte(nr)
-   err := ioutil.WriteFile(fileName, content, 0644)
-   if err != nil {
-      panic(err)
-   }
-}
-func Reader(fileName string) string{
-    
-   file, err := os.Open(fileName)
-   if err != nil {
-      panic(err)
-   }
-   defer file.Close()
-   content, err := ioutil.ReadAll(file)
-  // fmt.Println(string(content))
-  result := string(content)
-   return result
-    
-}
 
-/*
-golang判断文件或文件夹是否存在的方法为使用os.Stat()函数返回的错误值进行判断:
-如果返回的错误为nil,说明文件或文件夹存在
-如果返回的错误类型使用os.IsNotExist()判断为true,说明文件或文件夹不存在
-如果返回的错误为其它类型,则不确定是否在存在
-func PathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+func Create(fileName string, nr string) {
+	content := []byte(nr)
+	err := ioutil.WriteFile(fileName, content, 0644)
+	if err != nil {
+		panic(err)
 	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
 }
-*/
+func Reader(fileName string) string {
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	//log.Print(string(content))
+	result := string(content)
+	return result
+}
 func Exists(path string) bool {
-	_, err := os.Stat(path)    //os.Stat获取文件信息=> false :bcz true :cz
+	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
 	}
 	return true
 }
 
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
+}
 
-
-
-
-
-
-
-
-
-
-
-
+func IsFile(path string) bool {
+	return !IsDir(path)
+}
